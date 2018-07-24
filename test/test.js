2913dea96d26be71f0f201be4daf50896a018903
I048564 <Hariprasauth.R@sap.com>
2018-07-24 20:12:48 +0530

    Foodstuffs demo: Clearing the test case

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 var assert = require('assert');
 describe('Array', function() {
   describe('#indexOf()', function() {
-    it('-------------------------------- \n Unit Test case performed here \n --------------------------------', function() {
+    it('-------------------------------- \n \t \t Unit Test case performed here \n \t \t --------------------------------', function() {
       assert.equal([1,2,3].indexOf(4), -1);
     });
   });

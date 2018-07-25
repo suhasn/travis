1bb1e4875faf6b4d60ca0126d7a66a48ae67f076
I048564 <Hariprasauth.R@sap.com>
2018-07-25 07:20:58 +0530

    Sample change

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Hello World - This is awesome' });
+  res.render('index', { title: 'Hello World - This is awesome. This is great!' });
 });
 
 module.exports = router;
